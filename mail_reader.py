0bc60bd415ac1b43e391f894caf8aad789ec5b14
scamposr020 <44477230+scamposr020@users.noreply.github.com>
2025-12-19 09:55:48 -0600

    Update mail_reader.py

--- mail_reader.py
+++ mail_reader.py
@@ -4,26 +4,24 @@ import os
 import time
 
 
-def wait_for_otp_email(timeout_seconds=120, poll_interval=10):
-    imap_server = os.environ["OUTLOOK_IMAP_SERVER"]
-    email_user = os.environ["OUTLOOK_EMAIL_USER"]
-    email_password = os.environ["OUTLOOK_EMAIL_PASSWORD"]
+def wait_for_otp_email(timeout_seconds=180, poll_interval=10):
+    mail = imaplib.IMAP4_SSL(os.environ["OUTLOOK_IMAP_SERVER"])
+    mail.login(
+        os.environ["OUTLOOK_EMAIL_USER"],
+        os.environ["OUTLOOK_EMAIL_PASSWORD"],
+    )
 
     end_time = time.time() + timeout_seconds
 
-    mail = imaplib.IMAP4_SSL(imap_server)
-    mail.login(email_user, email_password)
-
     while time.time() < end_time:
         mail.select("inbox")
-
         status, messages = mail.search(None, '(UNSEEN)')
+
         if status != "OK":
             time.sleep(poll_interval)
             continue
 
-        email_ids = messages[0].split()
-        for eid in email_ids[::-1]:
+        for eid in messages[0].split()[::-1]:
             _, msg_data = mail.fetch(eid, "(RFC822)")
             msg = email.message_from_bytes(msg_data[0][1])
 
